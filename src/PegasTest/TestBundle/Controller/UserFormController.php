c455920f2a177723791afb63fb34af52df33e37a
pegastest <test@test.ru>
2014-07-03 11:49:32 +0400

    user form page

---
+++ src/PegasTest/TestBundle/Controller/UserFormController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace PegasTest\TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserFormController extends Controller
+{
+
+	public function indexAction()
+	{
+
+		$data=array();
+		//return new Response('<html><body>Hello '.'ololo'.'!</body></html>');
+		return $this->render('PegasTestTestBundle:UserForm:index.html.twig', array(
+
+		));
+
+	}
+}
+
