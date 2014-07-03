03a2ae135f814e45db072a88907c657c3ad81a50
pegastest <test@test.ru>
2014-07-03 11:03:24 +0400

    777 проставил. пускай так

--- web/app.php
+++ web/app.php
@@ -2,7 +2,7 @@
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
-
+umask(0000); // This will let the permissions be 0775
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance.
